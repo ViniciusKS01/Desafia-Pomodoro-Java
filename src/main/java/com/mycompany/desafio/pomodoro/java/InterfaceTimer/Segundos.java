d072f4bdefe83bce1c8f60f19ae3d0723b536258
ViniciusKS01 <martinsv016@gmail.com>
2020-12-02 01:03:04 -0400

    Criei um timer para os segundos

---
+++ src/main/java/com/mycompany/desafio/pomodoro/java/InterfaceTimer/Segundos.java
@@ -0,0 +1,40 @@
+package com.mycompany.desafio.pomodoro.java.InterfaceTimer;
+
+/**
+ *
+ * @author Vinícius Ivan
+ */
+public class Segundos implements Runnable {
+
+    private static boolean exit = false;
+
+    @Override
+    public void run() {
+        int i = 1;
+        int limite = Integer.parseInt(InterfaceTimer.txtMin.getText());
+        int aux = Integer.parseInt(InterfaceTimer.txtSec.getText());
+        while (!exit) {
+            InterfaceTimer.txtSec.setText(aux + "");
+            aux--;
+            if (aux < 0) {
+                try {
+                    if (i > limite) {
+                        this.stop();
+                    }
+                    i++;
+                } catch (Exception e) {
+                }
+                aux = 59;
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (Exception ex) {
+
+            }
+        }
+    }
+
+    static void stop() {
+        exit = true;
+    }
+}
